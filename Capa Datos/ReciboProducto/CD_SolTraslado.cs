3ba3b684c4fe11554f3df3acd3e0989929d267a4
subdirector.ti <subdirector.ti@CEDIS-STI>
2024-08-13 16:34:03 -0700

    Creacion de forma para recibo de TSR replicados de SAP

---
+++ Capa Datos/ReciboProducto/CD_SolTraslado.cs
@@ -0,0 +1,39 @@
+﻿using Capa_Entidad.Productos;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos.ReciboProducto
+{
+    public class CD_SolTraslado
+    {
+
+        private readonly CD_Conexion? conn = new CD_Conexion();
+
+        #region CargarSolicitudTraslado
+        public DataTable CargarSolTraslado(int NoTrs)
+        {
+            string sql = $@"SP_RP_CargarSolTraslado";
+            MySqlCommand cmd = new MySqlCommand(sql, conn.AbrirConexion());
+            cmd.Parameters.Add("@pTsr", MySqlDbType.Int32);
+
+            cmd.Parameters["@pTsr"].Value = NoTrs; // 171390;
+
+            // MySqlDataAdapter da = new MySqlDataAdapter(sql_select, conn.AbrirConexion());
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataSet ds = new DataSet();
+            ds.Clear();
+            da.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            conn.CerrarConexion();
+
+            return dt;
+        }
+        #endregion
+    }
+}
--- PuntoVentaCCFN/Views/EntradaRecibo.xaml.cs
+++ PuntoVentaCCFN/Views/EntradaRecibo.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Capa_Datos.ReciboProducto;
+using Capa_Negocio.ReciboProducto;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +10,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,11 +22,105 @@ namespace PuntoVentaCCFN.Views
     /// <summary>
     /// Lógica de interacción para EntradaRecibo.xaml
     /// </summary>
+    /// 
+    
+
     public partial class EntradaRecibo : System.Windows.Controls.UserControl
     {
+        readonly CN_SolTraslado objeto_CN_SolTraslado = new CN_SolTraslado();
+        readonly CD_ProcesarRecibo objeto_CD_ProcesarRecibo = new CD_ProcesarRecibo();
+        private bool nonNumberEntered = false;
+        public DataTable dt = null;
+        public string sMensaje = null;
+
         public EntradaRecibo()
         {
             InitializeComponent();
+        }
+
+        private void btn_Consultar_Click(object sender, RoutedEventArgs e)
+        {
+            RefrescarGrid();
+        }
+
+        private void RefrescarGrid()
+        { 
+
+            int iNoTst = 0;
+
+            try
+            {
+                iNoTst = Convert.ToInt32(tb_tsr.Text);
+            }
+            catch (Exception ex) 
+            {
+                System.Windows.MessageBox.Show("Invalido Numero de TSR\n" + ex.Message);
+                return;
+            }
+
+            dt = objeto_CN_SolTraslado.CargarSolTraslado(iNoTst);
+
+            GridSt.ItemsSource = dt.DefaultView; // objeto_CN_SolTraslado.CargarSolTraslado(iNoTst).DefaultView;
+
+            for (int i = 0; GridSt.Columns.Count > i; i++)
+            {
+                GridSt.Columns[i].IsReadOnly = true;
+            }
+
+           
+          //  GridSt.RowStyleSelector =  SingleLine;
+
+            GridSt.Columns[0].Header = "Linea";
+            GridSt.Columns[1].Header = "Estatus";
+            GridSt.Columns[2].Header = "Codigo";
+            GridSt.Columns[3].Header = "Descripción";
+            GridSt.Columns[4].Header = "Cantidad";
+            GridSt.Columns[5].Header = "UmTsr";
+            GridSt.Columns[6].Header = "Costo";
+            GridSt.Columns[7].Header = "Total";
+            GridSt.Columns[8].Header = "Moneda";
+            GridSt.Columns[9].Header = "Saldo";
+            
+            GridSt.Columns[10].Header = "Cant.Recibo";
+            GridSt.CellEditEnding += GridSt_CellEditEnding;
+            GridSt.Columns[10].IsReadOnly = false;
+         //  -- GridSt.Columns[10].header
+
+            GridSt.Columns[11].Header = "Base";
+            GridSt.Columns[12].Header = "Umi";
+            GridSt.Columns[13].Header = "Almacén";
+
+            GridSt.Columns[14].Visibility = 0; // DocEntry
+            GridSt.Columns[15].Visibility = 0; // UmEntry;
+
+            //string svalue = GridSt.Items[0][1].ToString();
+
+
+        }
+
+        private void GridSt_CellEditEnding(object? sender, DataGridCellEditEndingEventArgs e)
+        {
+           
+           
+        }
+
+        private void tb_tsr_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            
+
+        }
+
+        private void BtnProcesar_Click(object sender, RoutedEventArgs e)
+        {
+            if (objeto_CD_ProcesarRecibo.CD_ProcesarSolTraslado(ref dt, ref sMensaje))
+            {
+                System.Windows.MessageBox.Show("Proceso exitoso !!!");
+                RefrescarGrid();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show(sMensaje);
+            }
         }
     }
 }
