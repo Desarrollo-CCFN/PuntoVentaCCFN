def995cca7fc38737ce93565eec1415f319bbadd
subdirector.ti <subdirector.ti@CEDIS-STI>
2024-10-08 13:58:09 -0700

    cambios cierre de caja

--- PuntoVentaCCFN/Views/CierreCaja.xaml.cs
+++ PuntoVentaCCFN/Views/CierreCaja.xaml.cs
@@ -31,10 +31,22 @@ namespace PuntoVentaCCFN.Views
         Configuration AppConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
         readonly CN_VentaCaja objCNVentaCaja = new CN_VentaCaja();
         CE_VentaCaja infoCaja = new CE_VentaCaja();
+        
+        public int UserSupervisor;
+
+        
+         //public CierreCaja(int iSupervisor)
         public CierreCaja()
         {
+            
             InitializeComponent();
             CargarHeader();
+
+            tb_Qty_Debito.Text   = "0";
+            tb_qty_Credito.Text  = "0";
+            tb_totalDebito.Text  = "0";
+            tb_TotalCredito.Text = "0";
+
         }
 
         public void CargarHeader()
@@ -47,9 +59,113 @@ namespace PuntoVentaCCFN.Views
 
             infoCaja = objCNVentaCaja.infoCaja(Nom_Cajera.Num_Cajera, SucursalString.Trim(), nombreCajaInt);
 
-            GridHeader.ItemsSource = obC.CargaHeader(infoCaja.IdCash).DefaultView;
-            GriDDetalle.ItemsSource = obC.CargarDetalle(infoCaja.IdCash).DefaultView;
+            if (infoCaja != null)
+            {
+                GridHeader.ItemsSource = obC.CargaHeader(infoCaja.IdCash).DefaultView;
+                GriDDetalle.ItemsSource = obC.CargarDetalle(infoCaja.IdCash).DefaultView;
+            }
+            else
+            {
+                tb_Qty_Debito.IsEnabled = false;
+                tb_qty_Credito.IsEnabled = false;
+                tb_totalDebito.IsEnabled = false;
+                tb_TotalCredito.IsEnabled = false;
+                btn_Preview.IsEnabled = false;
+                btn_Procesar.IsEnabled = false;
+
+                System.Windows.MessageBox.Show("No existe caja abierta");
+
+            }
         }
 
+        private void btn_Preview_Click(object sender, RoutedEventArgs e)
+        {
+            CerrarCaja(true);
+        }
+
+        public void CerrarCaja(bool Preview)
+        {
+            
+            Int32  iQtyDebito = 0;
+            double totalDebito = 0;
+            Int32  iQtyCredito = 0;
+            double TotalCredito = 0;
+
+            try
+            {
+                iQtyDebito = Convert.ToInt32(tb_Qty_Debito.Text);
+                iQtyCredito = Convert.ToInt32(tb_qty_Credito.Text);
+                totalDebito = Convert.ToDouble(tb_totalDebito.Text);
+                TotalCredito = Convert.ToDouble(tb_TotalCredito.Text);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+                return;
+            }
+
+            try
+            {
+                var SettingSection = AppConfig.GetSection("App_Preferences") as Capa_Presentacion.App_Preferences;
+                string nombreCajaString = MainWindow.AppConfig1.Caja;
+                string SucursalString = SettingSection.Filler;
+                string NombreCompany = SettingSection.CompanyName;
+                int nombreCajaInt = int.Parse(nombreCajaString);
+
+                infoCaja = objCNVentaCaja.infoCaja(Nom_Cajera.Num_Cajera, SucursalString.Trim(), nombreCajaInt);
+
+                string CerrarCaja = "Y";
+                if (Preview == true)
+                {
+                     CerrarCaja = "N";
+                }
+                
+
+                if (infoCaja != null)
+                {
+                    string sMensaje = "";
+
+                    if (obC.CierraCaja(infoCaja.IdCash, totalDebito, TotalCredito, iQtyDebito, iQtyCredito, UserSupervisor, CerrarCaja, ref sMensaje) == false)
+                    {
+                        System.Windows.MessageBox.Show(sMensaje);
+                    }
+                    else
+                    {
+                        if (CerrarCaja == "Y")
+                        {
+                            System.Windows.MessageBox.Show("Proceso Exitoso !!!");
+                        }
+                    }
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("No existe caja abierta");
+                }
+            
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+                return;
+            }
+
+        }
+
+        private void btn_Cerrar_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btn_Procesar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult dialogResult = System.Windows.MessageBox.Show("Esta seguro de cerrar la caja", "Cierre de Caja", System.Windows.MessageBoxButton.YesNo, MessageBoxImage.Question);
+            
+            
+            if (dialogResult == MessageBoxResult.Yes)
+            {
+                CerrarCaja(false);
+            }
+           
+        }
     }
 }
