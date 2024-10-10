2c910702fbfa1bdcc255aee6e8adc397d1ad5cd9
Desarrollo-CCFN <desarrollo@superchivas.com.mx>
2024-10-10 13:47:28 -0700

    Terminado Reimpresiones y Cierre (impresion)

--- Capa Datos/CD_BusquedaReporte.cs
+++ Capa Datos/CD_BusquedaReporte.cs
@@ -1,5 +1,6 @@
 ﻿using Capa_Entidad;
 using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -14,20 +15,27 @@ namespace Capa_Datos
         private readonly CD_Conexion? conn = new CD_Conexion();
         private CE_BusquedaReporte ce = new CE_BusquedaReporte();
 
-        public CE_BusquedaReporte Consulta()
+        public CE_BusquedaReporte Consulta(int IdTra, int Param)
         {
             MySqlDataAdapter da = new MySqlDataAdapter("SP_C_BusquedaReporte", conn.AbrirConexion());
             da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            // Pasar parámetros al procedimiento almacenado
+            da.SelectCommand.Parameters.AddWithValue("_IdTran", IdTra);
+            da.SelectCommand.Parameters.AddWithValue("_Opc", Param);
+
+
             DataSet ds = new DataSet();
             ds.Clear();
             da.Fill(ds);
-            DataTable dt;
-            dt = ds.Tables[0];
-            DataRow row = dt.Rows[0];
 
-            ce.IdTr_ = Convert.ToInt32(row[0]);
-            ce.opc_ = Convert.ToInt32(row[1]);
-
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                DataRow row = ds.Tables[0].Rows[0];
+                ce.IdTr_ = Convert.ToInt32(row[0]);
+                ce.opc_ = Convert.ToInt32(row[1]);
+            }
+             
             return ce;
 
         }
