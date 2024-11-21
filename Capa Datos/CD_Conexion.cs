0cf3d94e8f953eadd4dd270781bdf4337545d845
Desarrollo-CCFN <desarrollo@superchivas.com.mx>
2024-11-20 16:48:14 -0800

    cambio direccion

--- Capa Datos/CD_Conexion.cs
+++ Capa Datos/CD_Conexion.cs
@@ -7,8 +7,8 @@ namespace Capa_Datos
     public static class Configuracion
     {
         // Cadena de conexión a la base de datos
-         public static readonly string CadenaConexion = "server=192.168.101.7;uid=desarrollo2; pwd=Chivas.2024;database=ccfn_desarrollo;";
-        //  public static readonly string CadenaConexion = "server=10.101.1.130;uid=root; pwd=root.2024;database=db_s12;";
+         // public static readonly string CadenaConexion = "server=192.168.101.7;uid=desarrollo2; pwd=Chivas.2024;database=ccfn_desarrollo;";
+          public static readonly string CadenaConexion = "server=10.101.1.130;uid=root; pwd=root.2024;database=db_s12;";
         // public static readonly string CadenaConexion = "server=localhost;uid=root; pwd=root.2024;database=db_s12;";
 
 
