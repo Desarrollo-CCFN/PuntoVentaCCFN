ca45a60b9b906ab9f95da12234d32912278b3d29
Anthony-Angulo <anthony.angulo@uabc.edu.mx>
2024-11-22 10:59:08 -0800

    Devoluciones header

---
+++ Capa Negocio/CN_Devoluciones.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using Capa_Datos;
+using Capa_Entidad.Devoluciones;
+
+
+namespace Capa_Negocio
+{
+    public class CN_Devoluciones
+    {
+        private readonly CD_Devoluciones obDatos = new CD_Devoluciones();
+
+        public CE_DevolucionHeader CargarHeader(string NumTicket) { 
+            return obDatos.CargarHeader(NumTicket);
+        }
+
+        public DataTable CargarDetalle(string NumTicket)
+        {
+            return obDatos.CargarDetalle(NumTicket);
+        }
+
+        public bool DevoluacionHeader(int _idHeader, int __UserId, ref string sMensaje)
+        {
+            return obDatos.DevolucionHeader(_idHeader, __UserId, ref sMensaje);
+        }
+    }
+}
--- PuntoVentaCCFN/Views/Devoluciones.xaml.cs
+++ PuntoVentaCCFN/Views/Devoluciones.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Capa_Entidad.Devoluciones;
+using Capa_Negocio;
+using PuntoVentaCCFN;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,83 @@ namespace Capa_Presentacion.Views
     /// </summary>
     public partial class Devoluciones : System.Windows.Controls.UserControl
     {
+        readonly CN_Devoluciones _oDevoluciones = new CN_Devoluciones();
+
+        CE_DevolucionHeader _oDevolucionHeader = new CE_DevolucionHeader();
+        CE_DevolucionDetalle _oDevolucionDetalle = new CE_DevolucionDetalle();
+
+        public string sMensaje = null;
         public Devoluciones()
         {
             InitializeComponent();
+        }
+
+        private void CargarHeader(object sender, RoutedEventArgs e)
+        {
+            if(tbNumTicket.Text == "") {
+                System.Windows.MessageBox.Show("Debes ingresar un numero de recibo!!");
+                return;
+            }
+
+             _oDevolucionHeader = _oDevoluciones.CargarHeader(tbNumTicket.Text);
+
+            if(_oDevolucionHeader == null)
+            {
+                System.Windows.MessageBox.Show("Venta no encontrada en el rango de fechas!!");
+                return;
+            }
+
+            lblRecibo.Text = _oDevolucionHeader.NumTck;
+            lblFecha.Text = _oDevolucionHeader.Fecha;
+            lblMoneda.Text = _oDevolucionHeader.DocCur;
+            lblRate.Text = _oDevolucionHeader.DocRate.ToString();
+            lblMXN.Text = _oDevolucionHeader.DocTotal.ToString();
+            lblUSD.Text = _oDevolucionHeader.DocTotalFC.ToString();
+
+            CargarDetalle();
+
+        }
+
+        private void CargarDetalle()
+        {
+            GridDatos.ItemsSource = _oDevoluciones.CargarDetalle(tbNumTicket.Text).DefaultView;
+
+        }
+
+        public void getSelectedItems()
+        {
+            if(GridDatos.SelectedItems.Count == 0) {
+                System.Windows.MessageBox.Show("Debes seleccionar productos a devolver!!");
+                return;
+            }
+
+            for(int i = 0; i < GridDatos.SelectedItems.Count; i++)
+            {
+                DataRowView dataRowView = (DataRowView)GridDatos.SelectedItems[i];
+
+            }
+
+            if(!_oDevoluciones.DevoluacionHeader(_oDevolucionHeader.Id, int.Parse(MainWindow.AppConfig1.Caja), ref sMensaje)) {
+                System.Windows.MessageBox.Show(sMensaje);
+                return;
+            }
+
+            System.Windows.MessageBox.Show("Exito!!");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            getSelectedItems();
+        }
+
+        public class GridList
+        {
+            public string ItemCode { get; set; }
+            public string Dscription { get; set; }
+            public string Unidad { get; set; }
+            public decimal Quantiy { get; set; }
+            public decimal LineTotal { get; set; }
+            public decimal LineTotalFrgn { get; set; }
         }
     }
 }
